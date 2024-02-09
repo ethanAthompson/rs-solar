3c52fa36ce9d3fb2e82295db095c99ef856f442c
ethanAthompson <ethan.a.thompson@outlook.com>
2024-02-08 22:11:23 -0500

    link update!

--- src/lib.rs
+++ src/lib.rs
@@ -19,21 +19,21 @@
 //! in their application.
 //!
 //! # References Used
-//! (Latex Editor)[https://latexeditor.lagrida.com/]
-//! (Desmos Scientific Calculator)[https://www.desmos.com/scientific]
-//! (Mars Html Calendar)[https://www-mars.lmd.jussieu.fr/mars/time/martian_time.html]
-//! (Design of an Elliptical Transfer Orbit)[https://space.stackexchange.com/questions/23128/design-of-an-elliptical-transfer-orbit/23130#23130]
-//! (Kepler Orbits)[https://www.bogan.ca/orbits/kepler/orbteqtn.html]
-//! (Hyperbolic Kepler's Equation)[https://control.asu.edu/Classes/MAE462/462Lecture05.pdf]
-//! (Semi Major Axises and Semi Minor Axises)[https://en.wikipedia.org/wiki/Semi-major_and_semi-minor_axes]
-//! (Geometry of Orbits)[https://www.bogan.ca/orbits/geometry.html]
-//! (Julian Date Converter)[https://aa.usno.navy.mil/data/JulianDate]
-//! - (Semi Latus Rectum of Parabola or Hyperbola)[https://www.orbiter-forum.com/threads/how-calculate-semi-latus-rectum-of-parabola-or-hyperbola.40315/]
+//! [Latex Editor](https://latexeditor.lagrida.com/)
+//! [Desmos Scientific Calculator](https://www.desmos.com/scientific)
+//! [Mars Html Calendar](https://www-mars.lmd.jussieu.fr/mars/time/martian_time.html)
+//! [Design of an Elliptical Transfer Orbit](https://space.stackexchange.com/questions/23128/design-of-an-elliptical-transfer-orbit/23130#23130)
+//! [Kepler Orbits](https://www.bogan.ca/orbits/kepler/orbteqtn.html)
+//! [Hyperbolic Kepler's Equation](https://control.asu.edu/Classes/MAE462/462Lecture05.pdf)
+//! [Semi Major Axises and Semi Minor Axises](https://en.wikipedia.org/wiki/Semi-major_and_semi-minor_axes)
+//! [Geometry of Orbits](https://www.bogan.ca/orbits/geometry.html)
+//! [Julian Date Converter](https://aa.usno.navy.mil/data/JulianDate)
+//! [Semi Latus Rectum of Parabola or Hyperbola](https://www.orbiter-forum.com/threads/how-calculate-semi-latus-rectum-of-parabola-or-hyperbola.40315/)
 //!     > Semi-Latus Rectum equation by Kolodez
-//! (Orbital Periods)[https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Solar_system_orbital_period_vs_semimajor_axis.svg/800px-Solar_system_orbital_period_vs_semimajor_axis.svg.png]
-//! (Astronomy Calculations)[https://docs.google.com/spreadsheets/d/1rwc2mVxyHuUEou_hxnG6kzl24XdqqmIAS5_1nJDpJ6o/edit#gid=1479831395]
-//! (Mars Clock)[https://marsclock.com/]
-//! (Orbits)[https://jtauber.github.io/orbits/]
+//! [Orbital Periods](https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Solar_system_orbital_period_vs_semimajor_axis.svg/800px-Solar_system_orbital_period_vs_semimajor_axis.svg.png)
+//! [Astronomy Calculations](https://docs.google.com/spreadsheets/d/1rwc2mVxyHuUEou_hxnG6kzl24XdqqmIAS5_1nJDpJ6o/edit#gid=1479831395)
+//! [Mars Clock](https://marsclock.com/)
+//! [Orbits](https://jtauber.github.io/orbits/)
 //!
 //!
 //! # Features
